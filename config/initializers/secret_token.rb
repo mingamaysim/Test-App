53d3bf47ea38a63fc13eb573bdb7d694456b6570
Ming Chen <ming.chen@amaysim.com.au>
2011-12-04 11:09:22 +1100

    blog base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'cece9afb3572faac4ec03146f3b292beb53480f7f7cccd3034a18d49399d741b57c59abb3ded84c3001d968cf7e72f7ddaf02007441ef1520809179db5b1ca7e'
